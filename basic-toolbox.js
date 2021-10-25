35b3f43b62de0d89873583fd537e599241d69d65
Serhat Ozturk <so2turk@gmail.com>
2021-10-25 02:18:19 +0200

    Correct typo

--- basic-toolbox.js
+++ basic-toolbox.js
@@ -1,7 +1,7 @@
-const arrEqual = require('../src/arr-equal.js')
-const arrSort = require('../src/arr-sort.js')
-const randomInt = require('../src/random-int.js')
-const round = require('../src/round.js')
+const arrEqual = require('./src/arr-equal.js')
+const arrSort = require('./src/arr-sort.js')
+const randomInt = require('./src/random-int.js')
+const round = require('./src/round.js')
 
 module.exports = {
     arrEqual,
